d1a8a6b0e1e9aabeb7a97c8ea6dcddaf12b0c4c4
Leah Nelson <leahcnels@gmail.com>
2016-09-09 14:25:12 -0700

    add delete action for images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -10,12 +10,19 @@ class ImagesController < ApplicationController
     @photographer = Photographer.find(params[:photographer_id])
     @image = @photographer.images.new(image_params)
     if @image.save
+      flash[:success] = "Image uploaded!"
       redirect_to photographer_path(@photographer)
     else
+      flash[:alert] = "Upload unsuccessful, please try again!"
       render :new
     end
   end
-
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    flash[:error] = "Image deleted!"
+    redirect_to photographer_path(@image.photographer)
+  end
 private
   def image_params
     params.require(:image).permit(:img_file)
