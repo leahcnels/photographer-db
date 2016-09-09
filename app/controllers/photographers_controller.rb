72f887fc497b1ba1e07a4c824a5e39560374228d
Leah Nelson <leahcnels@gmail.com>
2016-09-09 13:00:17 -0700

    add flash alerts with helpers to change colors

--- app/controllers/photographers_controller.rb
+++ app/controllers/photographers_controller.rb
@@ -11,8 +11,10 @@ class PhotographersController < ApplicationController
   def create
     @photographer = Photographer.new(photographer_params)
     if @photographer.save
+      flash[:success] = "Photographer added!"
       redirect_to photographers_path
     else
+      flash[:alert] = "Save unsuccessful, please try again!"
       render :new
     end
   end
@@ -22,14 +24,17 @@ class PhotographersController < ApplicationController
   def update
     @photographer = Photographer.find(params[:id])
     if @photographer.update(photographer_params)
+      flash[:success] = "Photographer updated!"
       redirect_to photographer_path(@photographer)
     else
+      flash[:alert] = "Save unsuccessful, please try again!"
       render :edit
     end
   end
   def destroy
     @photographer = Photographer.find(params[:id])
     @photographer.destroy
+    flash[:error] = "Photographer deleted!"
     redirect_to photographers_path
   end
 
