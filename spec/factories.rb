d37e31e84323abe373a37de940934ad53a2f5c86
Leah Nelson <leahcnels@gmail.com>
2016-09-16 10:55:33 -0700

    add navbar

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name 'Leah'
+    email 'leahcnels@gmail.com'
+    password 'password'
+  end
+end
---
+++ spec/features/log_in_pages_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+describe "the log in process" do
+end
