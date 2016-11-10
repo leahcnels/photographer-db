bb4b7c2e086005091341887c31eaf4fe7bccd521
Leah Nelson <leahcnels@gmail.com>
2016-11-09 20:33:32 -0800

    add dropdown to nav bar and first attempt to make cloudfront work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,9 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    :s3_host_name => 's3-us-west-2.amazonaws.com',
+    :url => ':s3_alias_url',
+    :s3_host_alias => 'd3s059iakl3pjo.cloudfront.net',
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
     :s3_protocol => :https,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
